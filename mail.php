be774be4f9d58948466b9d2126a324f2c5f7b5b1
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-05-20 19:36:48 +0400

    20-05

--- mail.php
+++ mail.php
@@ -1,51 +1,29 @@
 <?php
-if (isset ($_POST['useremail'])) {
-  $to = "vasyarovnov0802@gmail.com";
-  $from = "5.35.87.68";
-  $subject = "Заявка оставлена на сайте ".$_SERVER['HTTP_REFERER'];
-  $output='';
 
-  echo $output;
-  $message = "Имя : ".$_POST['username']              
-              ."\nПочта".$_POST['useremail']              
-              ."\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-  $boundary = md5(date('r', time()));
-  $filesize = '';
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers .= "From: " . $from . "\r\n";
-  $headers .= "Reply-To: " . $from . "\r\n";
-  $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
-  $message="
-Content-Type: multipart/mixed; boundary=\"$boundary\"
+//Script Foreach
+$c = true;
+if ( $method === 'POST' ) {
 
---$boundary
-Content-Type: text/plain; charset=\"utf-8\"
-Content-Transfer-Encoding: 7bit
+	$project_name = trim($_POST["username"]);
+	$admin_email  = trim($_POST["useremail"]);
 
-$message";
-     if(is_uploaded_file($_FILES['file-input']['tmp_name'])) {
-         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['file-input']['tmp_name'])));
-         $filename = $_FILES['file-input']['name'];
-         $filetype = $_FILES['file-input']['type'];
-         $filesize = $_FILES['file-input']['size'];
-         $message.="
 
---$boundary
-Content-Type: \"$filetype\"; name=\"$filename\"
-Content-Transfer-Encoding: base64
-Content-Disposition: attachment; filename=\"$filename\"
+	
+} else if ( $method === 'GET' ) {
 
-$attachment";
-     }
-   $message.="
---$boundary--";
-
-  if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
-    mail($to, $subject, $message, $headers);
-    /* echo $_POST['name'].', Ваше сообщение отправлено, спасибо!'; */
-  } else {
-    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
-  }
+	$project_name = trim($_GET["project_name"]);
+	$admin_email  = trim($_GET["admin_email"]);
+	
 }
-?>
+
+$message = "<table style='width: 100%;'>'$admin_email - $project_name'</table>";
+
+
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.$project_name.' <'.$admin_email.'>' . PHP_EOL .
+'Reply-To: '.$admin_email.'' . PHP_EOL;
+
+mail($admin_email, $message, $headers );
