06e99fed0182d80b83d87a3ff40bc7ea58ea1312
Василий Ровнов <vasyarovnov0802@gmail.com>
2024-05-20 20:23:17 +0400

    20-05

--- dist/mail.php
+++ dist/mail.php
@@ -1,22 +1,22 @@
 <?php
-if (isset ($_POST['useremail'])) {
-  $to = "vasyarovnov0802@gmail.com";
-  $from = "5.35.87.68";
-  $subject = "Заявка оставлена на сайте ".$_SERVER['HTTP_REFERER'];
-  $output='';
 
-  echo $output;
-  $message = "Имя : ".$_POST['username']              
-              ."\nПочта".$_POST['useremail']              
-              ."\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
+$to = "vasyarovnov0802@gmail.com";
+$from = "vasyarovnov0802@gmail.com";
+$subject = "Заявка оставлена на сайте " . $_SERVER['HTTP_REFERER'];
+$output = '';
 
-  $boundary = md5(date('r', time()));
-  $filesize = '';
-  $headers = "MIME-Version: 1.0\r\n";
-  $headers .= "From: " . $from . "\r\n";
-  $headers .= "Reply-To: " . $from . "\r\n";
-  $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
-  $message="
+echo $output;
+$message = "Имя : " . $_POST['username']
+	. "\nПочта" . $_POST['useremail']
+	. "\n\nАдрес сайта: " . $_SERVER['HTTP_REFERER'];
+
+$boundary = md5(date('r', time()));
+$filesize = '';
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "From: " . $from . "\r\n";
+$headers .= "Reply-To: " . $from . "\r\n";
+$headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+$message = "
 Content-Type: multipart/mixed; boundary=\"$boundary\"
 
 --$boundary
@@ -24,12 +24,12 @@ Content-Type: text/plain; charset=\"utf-8\"
 Content-Transfer-Encoding: 7bit
 
 $message";
-     if(is_uploaded_file($_FILES['file-input']['tmp_name'])) {
-         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['file-input']['tmp_name'])));
-         $filename = $_FILES['file-input']['name'];
-         $filetype = $_FILES['file-input']['type'];
-         $filesize = $_FILES['file-input']['size'];
-         $message.="
+if (is_uploaded_file($_FILES['file-input']['tmp_name'])) {
+	$attachment = chunk_split(base64_encode(file_get_contents($_FILES['file-input']['tmp_name'])));
+	$filename = $_FILES['file-input']['name'];
+	$filetype = $_FILES['file-input']['type'];
+	$filesize = $_FILES['file-input']['size'];
+	$message .= "
 
 --$boundary
 Content-Type: \"$filetype\"; name=\"$filename\"
@@ -37,15 +37,13 @@ Content-Transfer-Encoding: base64
 Content-Disposition: attachment; filename=\"$filename\"
 
 $attachment";
-     }
-   $message.="
+}
+$message .= "
 --$boundary--";
 
-  if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
-    mail($to, $subject, $message, $headers);
-    /* echo $_POST['name'].', Ваше сообщение отправлено, спасибо!'; */
-  } else {
-    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
-  }
+if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
+	mail($to, $subject, $message, $headers);
+	/* echo $_POST['name'].', Ваше сообщение отправлено, спасибо!'; */
+} else {
+	echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
 }
-?>
